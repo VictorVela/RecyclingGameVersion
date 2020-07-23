ec3baa563fa1a612f34f927f84788adf1024e678
Victor Galhardo Vela <victorgvela@hotmail.com>
2020-07-23 06:10:31 -0300

    tela de login
    foi implementado uma tela de login para este projeto

---
+++ Assets/Login_score/ScoreSend.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreSend : MonoBehaviour
+{
+    public void SendScore (int pontuacao, int fase)
+    {
+        PlayerData data = SaveSystem.LoadPlayer();
+    }
+}
