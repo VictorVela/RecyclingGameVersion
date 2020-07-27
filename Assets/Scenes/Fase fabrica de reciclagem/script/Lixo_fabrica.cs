b61aa45f2855e47812368ae8998f7c96a3fcc6f2
Victor Galhardo Vela <victorgvela@hotmail.com>
2020-07-27 03:57:34 -0300

    fase de reciclagem
    bugs pequenos de pontuação

---
+++ Assets/Scenes/Fase fabrica de reciclagem/script/Lixo_fabrica.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class Lixo_fabrica : MonoBehaviour
+{
+    public float speed = 30.0f;
+    public Sprite lata, jornal, plastico, vidro, organico, entulho;
+    private SpriteRenderer rend;
+    private Rigidbody2D rb;
+    private int randomRange;
+    private bool normal = true;
+    private bool firstP = false;
+    private bool secondP = false;
+    private bool seta01 = false;
+    private bool destroy = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        randomRange = Random.Range(0,6);
+
+        rend = GetComponent<SpriteRenderer>();
+
+        if (randomRange == 0)
+        {
+            lata = Resources.Load<Sprite>("lata");
+            rend.sprite = lata;
+        }
+
+        if (randomRange == 1)
+        {
+            jornal = Resources.Load<Sprite>("jornal");
+            rend.sprite = jornal;
+        }
+
+        if (randomRange == 2)
+        {
+            plastico = Resources.Load<Sprite>("plastico");
+            rend.sprite = plastico;
+        }
+
+        if (randomRange == 3)
+        {
+            vidro = Resources.Load<Sprite>("vidro");
+            rend.sprite = vidro;
+        }
+
+        if (randomRange == 4)
+        {
+            organico = Resources.Load<Sprite>("organico");
+            rend.sprite = organico;
+        }
+
+        if (randomRange == 5)
+        {
+            entulho = Resources.Load<Sprite>("entulho");
+            rend.sprite = entulho;
+        }
+
+        if (randomRange == 6)
+        {
+            entulho = Resources.Load<Sprite>("entulho");
+            rend.sprite = entulho;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+
+        if (collision.tag == "FirstPoint")
+        {
+            normal = false;
+            firstP = true;
+        }
+
+        if (collision.tag == "SecondPoint")
+        {
+            normal = false;
+            firstP = false;
+            secondP = true;
+        }
+
+        //seta 01
+        if (collision.tag == "Seta")
+        {
+            seta01 = true;
+        }
+
+        if (collision.tag == "plastico_fab")
+        {
+            if (randomRange == 2)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }
+        }
+
+        if (collision.tag == "metal_fab")
+        {
+            if (randomRange == 0)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }
+        }
+
+        if (collision.tag == "papel_fab")
+        {
+            if (randomRange == 1)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }
+        }
+
+        if (collision.tag == "vidro_fab")
+        {
+            if (randomRange == 3)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }
+        }
+
+        if (collision.tag == "organico_fab")
+        {
+            if (randomRange == 4)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }
+        }
+
+        if (collision.tag == "FinalPoint")
+        {
+            if(randomRange == 5 || randomRange == 6)
+            {
+                fab_score.scoreValue += 10;
+                destroy = true;
+            }
+            else
+            {
+                fab_score.GameOver();
+            }                    
+        }
+    }
+
+    public void DestroyLixo()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // First point
+    public void FistPoint()
+    {
+        firstP = true;
+    }
+
+    // Scond point
+    public void SecondPoint()
+    {
+        secondP = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (normal == true)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+            // rb.velocity = new Vector2(-speed, 0);
+            rb.velocity = new Vector2(-speed, 0);
+        }
+
+        if (firstP == true)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(0, speed);
+        }
+
+        if (secondP == true)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(speed, 0);
+        }
+
+        // seta01
+        if (seta01 == true)
+        {
+            rb = this.GetComponent<Rigidbody2D>();
+            rb.velocity = new Vector2(0, speed);
+        }
+
+        if (destroy == true)
+        {
+            DestroyLixo();
+        }
+    }
+
+}
---
+++ Assets/Scenes/Fase fabrica de reciclagem/script/Seta_Button.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seta_Button : MonoBehaviour
+{
+    private SpriteRenderer rend;
+    public Sprite cima, reto;
+    public bool seta = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeButtonSprite()
+    {
+        // this object was clicked - do something
+        Debug.Log("Teste");
+        Seta_Button seta_Button = new Seta_Button();
+        seta_Button.seta = !seta_Button.seta;
+
+        if (seta_Button.seta)
+        {
+            seta_Button.cima = Resources.Load<Sprite>("cima");
+            seta_Button.rend.sprite = seta_Button.cima;
+            transform.gameObject.tag = "Seta";
+        }
+
+        if (seta_Button.seta == false)
+        {
+            seta_Button.reto = Resources.Load<Sprite>("reto");
+            seta_Button.rend.sprite = seta_Button.reto;
+            transform.gameObject.tag = "reto";
+        }
+    }
+}
