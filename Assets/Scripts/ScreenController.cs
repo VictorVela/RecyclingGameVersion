b224bdd60b0f7e80bf667a2ecf9c796817242de4
Endryck <endryck.willyan.ilt@gmail.com>
2020-08-09 23:38:21 -0300

    New update
    Button person select add

--- Assets/Scripts/ScreenController.cs
+++ Assets/Scripts/ScreenController.cs
@@ -8,6 +8,10 @@ public class ScreenController : MonoBehaviour
     public GameObject screen1;
     public GameObject screen2;
     public GameObject screen3;
+    public GameObject screen4;
+    public GameObject screen5;
+
+    private string levelName;
 
     void Start()
     {
@@ -31,12 +35,12 @@ public class ScreenController : MonoBehaviour
         screen2.GetComponent<Animator>().Play("FadeOutMore");
         screen1.GetComponent<Animator>().Play("FadeIn");
     }
-    public void SelectBoy(string levelName)
+    public void SelectBoy()
     {
         PersonSelect.selectedPerson = "Boy";
         SceneManager.LoadScene(levelName);
     }
-    public void SelectGirl(string levelName)
+    public void SelectGirl()
     {
         PersonSelect.selectedPerson = "Girl";
         SceneManager.LoadScene(levelName);
@@ -44,27 +48,35 @@ public class ScreenController : MonoBehaviour
 
     public void GameStart()
     {
-        //SceneManager.LoadScene("Fase01");
+        //SceneManager.LoadScene(levelName);
     }
 
     public void StartFase01()
     {
-        SceneManager.LoadScene("Fase01");
+        //SceneManager.LoadScene("Fase01");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase01";
     }
 
     public void StartFase02()
     {
-        SceneManager.LoadScene("Fase02");
+        //SceneManager.LoadScene("Fase02");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase02";
     }
 
     public void StartFase03()
     {
-        SceneManager.LoadScene("Fase03");
+        //SceneManager.LoadScene("Fase03");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase03";
     }
 
     public void StartFase04()
     {
-        SceneManager.LoadScene("Fase04");
+        //SceneManager.LoadScene("Fase04");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase04";
     }
     public void StartFase05()
     {
@@ -72,15 +84,36 @@ public class ScreenController : MonoBehaviour
     }
     public void StartFase06()
     {
-        SceneManager.LoadScene("Fase06");
+        //SceneManager.LoadScene("Fase06");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase06";
     }
     
     public void StartFase07()
     {
-        SceneManager.LoadScene("Fase07");
+        //SceneManager.LoadScene("Fase07");
+        screen3.GetComponent<Animator>().Play("FadeInPerson");
+        levelName = "Fase07";
     }
     public void StartFase08()
     {
-        screen1.GetComponent<Animator>().Play("FadeInPerson");
+        //screen1.GetComponent<Animator>().Play("FadeInPerson");
+    }
+
+    public void MouseInsideGirl()
+    {
+        screen4.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+    public void MouseOutsideGirl()
+    {
+        screen4.gameObject.GetComponent<CanvasGroup>().alpha = 1;
+    }
+    public void MouseInsideBoy()
+    {
+        screen5.gameObject.GetComponent<CanvasGroup>().alpha = 0;
+    }
+    public void MouseOutsideBoy()
+    {
+        screen5.gameObject.GetComponent<CanvasGroup>().alpha = 1;
     }
 }
